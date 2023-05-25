368f946f028b1d69a67f2806a2eae4ff1cdc8f2c
mor azogi <azogi@post.bgu.ac.il>
2023-05-25 14:26:56 +0300

    2.2.3 changes

--- src/SkipListExperimentUtils.java
+++ src/SkipListExperimentUtils.java
@@ -49,7 +49,7 @@ public class SkipListExperimentUtils {
     }
 
     public static double measureSearch(AbstractSkipList skipList, int size) {
-        Double timeElapsed = 0.0;
+        double timeElapsed = 0.0;
         boolean[] ex = new boolean[size];
         for (int i = 0 ; i < size; i++) {
             ex[i] = false;
@@ -89,104 +89,7 @@ public class SkipListExperimentUtils {
     }
 
     public static void main(String[] args) {
-//        double[] ans = new double[16];
-//        double t = 0.75;
-//        double ex = 0;
-//        double temp1;
-//        double temp2;
-//        double temp3;
-//        double temp4;
-//        double temp5;
-//        System.out.println(t+"*10 ---------------------");
-//        temp1 = measureLevels(t,10);
-//        System.out.print(temp1 +" - ");
-//        ex = ex + temp1;
-//        temp2 = measureLevels(t,10);
-//        System.out.print(temp2 +" - ");
-//        ex = ex + temp2;
-//        temp3 = measureLevels(t,10);
-//        System.out.print(temp3 +" - ");
-//        ex = ex + temp3;
-//        temp4 = measureLevels(t,10);
-//        System.out.print(temp4 +" - ");
-//        ex = ex + temp4;
-//        temp5 = measureLevels(t,10);
-//        System.out.println(temp5 +" - ");
-//        ex = ex + temp5;
-//        ex = ex*t;
-//        System.out.println("second = "+ex);
-//        double third =  ((ex-temp1) + (ex-temp2) + (ex-temp3) + (ex-temp4) + (ex-temp5));
-//        third = third/5;
-//        System.out.println("third = " + third);
-//        ex = 0;
-//
-//        System.out.println(t+"*100 ---------------------");
-//        temp1 = measureLevels(t,100);
-//        System.out.print(temp1 +" - ");
-//        ex = ex + temp1;
-//        temp2 = measureLevels(t,100);
-//        System.out.print(temp2 +" - ");
-//        ex = ex + temp2;
-//        temp3 = measureLevels(t,100);
-//        System.out.print(temp3 +" - ");
-//        ex = ex + temp3;
-//        temp4 = measureLevels(t,100);
-//        System.out.print(temp4 +" - ");
-//        ex = ex + temp4;
-//        temp5 = measureLevels(t,100);
-//        System.out.println(temp5 +" - ");
-//        ex = ex + temp5;
-//        ex = ex*t;
-//        System.out.println("second = "+ex);
-//        third =  ((ex-temp1) + (ex-temp2) + (ex-temp3) + (ex-temp4) + (ex-temp5));
-//        third = third/5;
-//        System.out.println("third = " + third);
-//        ex = 0;
-//
-//        System.out.println(t+"*1000 ---------------------");
-//        temp1 = measureLevels(t,1000);
-//        System.out.print(temp1 +" - ");
-//        ex = ex + temp1;
-//        temp2 = measureLevels(t,1000);
-//        System.out.print(temp2 +" - ");
-//        ex = ex + temp2;
-//        temp3 = measureLevels(t,1000);
-//        System.out.print(temp3 +" - ");
-//        ex = ex + temp3;
-//        temp4 = measureLevels(t,1000);
-//        System.out.print(temp4 +" - ");
-//        ex = ex + temp4;
-//        temp5 = measureLevels(t,1000);
-//        System.out.println(temp5 +" - ");
-//        ex = ex + temp5;
-//        double realex = ex*t;
-//        System.out.println("second = "+realex);
-//        third =  ((realex-temp1) + (realex-temp2) + (realex-temp3) + (realex-temp4) + (realex-temp5));
-//        third = third/5;
-//        System.out.println("third = " + third);
-//        ex = 0;
-//
-//        System.out.println(t+"*10000 ---------------------");
-//        temp1 = measureLevels(t,10000);
-//        System.out.print(temp1 +" - ");
-//        ex = ex + temp1;
-//        temp2 = measureLevels(t,10000);
-//        System.out.print(temp2 +" - ");
-//        ex = ex + temp2;
-//        temp3 = measureLevels(t,10000);
-//        System.out.print(temp3 +" - ");
-//        ex = ex + temp3;
-//        temp4 = measureLevels(t,10000);
-//        System.out.print(temp4 +" - ");
-//        ex = ex + temp4;
-//        temp5 = measureLevels(t,10000);
-//        System.out.println(temp5 +" - ");
-//        ex = ex + temp5;
-//        ex = ex*t;
-//        System.out.println("second = "+ex);
-//        third =  ((ex-temp1) + (ex-temp2) + (ex-temp3) + (ex-temp4) + (ex-temp5));
-//        third = third/5;
-//        System.out.println("third = " + third);
+
 
 //-----------
         int x = 1000;
@@ -203,12 +106,6 @@ public class SkipListExperimentUtils {
         System.out.println("insert - "+ (insert_expirement_sum/30));
         System.out.println("search - "+ (search_expirement_sum/30));
         System.out.println("deletion - "+ (deletion_expirement_sum/30));
-
-
-
-//        System.out.println(measureLevels(0.5,10));
-//        System.out.println(measureLevels(0.75,10));
-//        System.out.println(measureLevels(0.9,10));
 
     }
 }
