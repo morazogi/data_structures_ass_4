bed44027c3b536f66cc4077422542c01e17762a5
mor azogi <azogi@post.bgu.ac.il>
2023-06-04 18:30:30 +0300

    3.4

--- src/MultiplicativeShiftingHash.java
+++ src/MultiplicativeShiftingHash.java
@@ -1,31 +1,35 @@
-//import java.util.Random;
-//
-//public class MultiplicativeShiftingHash implements HashFactory<Long> {
-//    public MultiplicativeShiftingHash() {
-//        throw new UnsupportedOperationException("Replace this by your implementation");
-//    }
-//
-//    @Override
-//    public HashFunctor<Long> pickHash(int k) {
-//        throw new UnsupportedOperationException("Replace this by your implementation");
-//    }
-//
-//    public class Functor implements HashFunctor<Long> {
-//        final public static long WORD_SIZE = 64;
-//        final private long a;
-//        final private long k;
-//
-//        @Override
-//        public int hash(Long key) {
-//            throw new UnsupportedOperationException("Replace this by your implementation");
-//        }
-//
-//        public long a() {
-//            return a;
-//        }
-//
-//        public long k() {
-//            return k;
-//        }
-//    }
-//}
+import java.util.Random;
+
+public class MultiplicativeShiftingHash implements HashFactory<Long> {
+    private HashingUtils utils;
+    public MultiplicativeShiftingHash() {
+        this.utils = new HashingUtils();
+    }
+
+    @Override
+    public HashFunctor<Long> pickHash(int k) {
+        long a = utils.genLong(2,Long.MAX_VALUE);
+        return new Functor(a,k);
+    }
+
+    public class Functor implements HashFunctor<Long> {
+        final public static long WORD_SIZE = 64;
+        final private long a;
+        final private long k;
+
+        @Override
+        public int hash(Long key) {
+            return (int) (a*key)>>>(WORD_SIZE-k);
+        }
+        public long a() {
+            return a;
+        }
+        public long k() {
+            return k;
+        }
+        public Functor(long a, long k){
+            this.a = a;
+            this.k = k;
+        }
+    }
+}
