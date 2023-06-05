d38d3729bc93600aa0cbe0190cfa91e19dd5def6
mor azogi <azogi@post.bgu.ac.il>
2023-06-05 15:35:58 +0300

    3.7

--- src/ProbingHashTable.java
+++ src/ProbingHashTable.java
@@ -9,6 +9,7 @@ public class ProbingHashTable<K, V> implements HashTable<K, V> {
     private HashFunctor<K> hashFunc;
     private Pair<K,V>[] hash;
     private int size;
+    private HashingUtils utils;
     /*
      * You should add additional private members as needed.
      */
@@ -22,17 +23,60 @@ public class ProbingHashTable<K, V> implements HashTable<K, V> {
         this.hashFunc = hashFactory.pickHash(k);
         this.size = 0;
         this.hash = new Pair[capacity];
+        this.utils = new HashingUtils();
     }
     public V search(K key) {
         V value = null;
         int locationInHash = this.hashFunc.hash(key);
-
+        if (hash[locationInHash]==null) return null;
+        else
+        {
+            while (hash[locationInHash].second()==null
+                    &&hash[locationInHash].first()!=key){
+                locationInHash = utils.mod(locationInHash,capacity());
+                if (hash[locationInHash]==null) return null;
+            }
+            value = hash[locationInHash].second();
+        }
+        return value;
     }
     public void insert(K key, V value) {
-        throw new UnsupportedOperationException("Replace this by your implementation");
+        Pair add = new Pair(key, value);
+        int locationInHash = this.hashFunc.hash(key);
+        if (hash[locationInHash] == null) hash[locationInHash] = add;
+        else {
+            while (hash[locationInHash] != null) {
+                locationInHash = utils.mod(locationInHash, capacity());
+                if (hash[locationInHash] == null) hash[locationInHash] = add;
+            }
+        }
+        size++;
+        this.rehash();
+    }
+    public void rehash(){
+        ProbingHashTable rehashed = new ProbingHashTable(hashFactory,(int) (Math.log(capacity)+1), maxLoadFactor);
+        for (int i = 0; i < hash.length; i++) {
+            if (hash[i]!=null && hash[i].second()!=null){
+                rehashed.insert(hash[i].first(),hash[i].second());
+            }
+        }
+        this.capacity = rehashed.capacity();
+        this.hashFunc = rehashed.getHashFunc();
+        this.hash = rehashed.hash;
     }
     public boolean delete(K key) {
-        throw new UnsupportedOperationException("Replace this by your implementation");
+        int locationInHash = this.hashFunc.hash(key);
+        if (hash[locationInHash]==null) return false;
+        else
+        {
+            while (hash[locationInHash].second()==null
+                    &&hash[locationInHash].first()!=key){
+                locationInHash = utils.mod(locationInHash,capacity());
+                if (hash[locationInHash]==null) return false;
+            }
+            hash[locationInHash] = new Pair<>(key,null);
+        }
+        return true;
     }
     public HashFunctor<K> getHashFunc() {
         return hashFunc;
